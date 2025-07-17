10cf0fc798a58fcd6c6ded602bdde758fec148db
Glowing Jellyfishing <glowingjellyfishing@outlook.com>
2025-07-17 23:00:44 +0000

    Add initial structure and styling for Roblox fan site

---
+++ script.js
@@ -0,0 +1,8 @@
+function showInfo(game) {
+    let info = {
+        'Adopt Me!': 'Adopt Me! is a massively popular game where you can adopt pets, decorate your house, and trade with friends.',
+        'Brookhaven 🏡RP': 'Brookhaven is a roleplaying game where you can buy homes, drive vehicles, and live out your dream life.',
+        'Tower of Hell': 'Tower of Hell is a challenging obby game where you race against time and other players to reach the top.'
+    };
+    alert(info[game] || 'More info coming soon!');
+}
